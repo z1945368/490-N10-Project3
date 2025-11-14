184527fe87660827f347d156761fc63a99037aee
Jordan Glazier <118040783+dglaz300x4@users.noreply.github.com>
2025-11-13 23:00:42 -0600

    Updates to display popup

--- suspectFinderGUI/js/suspectReport.js
+++ suspectFinderGUI/js/suspectReport.js
@@ -1,10 +1,30 @@
+// Expand/Collapse list of reports for a given suspect
 const collapseSuspectLabelButton = document.getElementById("suspectLabel#");
 const collapseSuspectReports = document.getElementById("suspectReports#");
 
-collapseSuspectLabelButton.addEventListener(
+collapseSuspectLabelButton.addEventListener( // Listener to expand/collapse the report list.
     "click",
     function(){
-        collapseSuspectReports.classList.toggle("fullExtendedArea");
+        collapseSuspectReports.classList.toggle("extendableAreaExpand");
     }
+);
 
-)
+// Open/Close pop up window for report
+const reportLabelButton = document.getElementById("report#1");
+const popUpReturnButton = document.getElementById("returnButton");
+const popUpWindow = document.getElementById("reportPopUp");
+
+
+reportLabelButton.addEventListener( // Listener to open report pop up
+    "click",
+    function (){
+        popUpWindow.showModal(); // Open pop up on click
+    }
+);
+
+popUpReturnButton.addEventListener( // Listener to close the pop up
+    "click",
+    function(){
+        popUpWindow.close(); // Close pop up on click
+    } 
+);
