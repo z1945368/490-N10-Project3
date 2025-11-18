b556a77e80d29e484f63f25867200a4f141f855f
Jordan Glazier <118040783+dglaz300x4@users.noreply.github.com>
2025-11-17 20:45:27 -0600

    Report pop up displays info
    Once clicked, the report pop up displays info related to the report

--- suspectFinderGUI/js/fillSuspectList.js
+++ suspectFinderGUI/js/fillSuspectList.js
@@ -1,5 +1,6 @@
 // Link JSON to files
 var suspectReports = new Map();
+var fullReports = new Map();
 
 async function getJSONData(data) { // Get JSON data and send to html file
     const listBody = document.getElementById("suspectList");
@@ -16,6 +17,8 @@ function getSuspectList(personMap){ // Gets the list of the suspects
     var suspectListArray = Array.from(personMap);
 
     suspectListArray.sort((a,b) => b[1] - a[1]);
+
+    // suspectListArray = suspectListArray.filter(([name,value]) => value >= 2); // Remove all the suspects with a single report.
 
     return suspectListArray;
 }
@@ -53,31 +56,73 @@ function buildReportListItem(reportID){
 }
 
 function addButtonListenersToSuspects(){
-    
     suspectReports.forEach((reports, key)=>{
         const suspectLabelButton = document.getElementById(`suspectListItem${key}`);
         const suspectReportExpandable = document.getElementById(`suspectReports${key}`);
         const suspectReportList = document.getElementById(`reportList${key}`);
 
-        reports.forEach(report => {
-            suspectReportList.appendChild(buildReportListItem(report));
-        });
-
-        suspectLabelButton.addEventListener(
+        suspectLabelButton.addEventListener( // Turns label into drop down menu
             "click",
             function (){
                 suspectReportExpandable.classList.toggle("extendableAreaExpand");
             }
         );
 
+        reports.forEach(report => {
+            suspectReportList.appendChild(buildReportListItem(report));
+            const reportLabelButton = document.getElementById(`report${report}`);
+            const popUpReturnButton = document.getElementById("returnButton");
+            const popUpWindow = document.getElementById("reportPopUp");
+            
+            reportLabelButton.addEventListener(
+                "click",
+                function (){
+                    fillReportPopUp(fullReports.get(report));
+                    popUpWindow.showModal(); // Open pop up on click
+                }
+            );
+
+            popUpReturnButton.addEventListener( // Listener to close the pop up
+                "click",
+                function(){
+                    popUpWindow.close(); // Close pop up on click
+                } 
+            );
+        });
     });
 
+
+}
+
+function fillReportPopUp(report){
+    const popUpTitle = document.getElementById("reportTitleID");
+    const popUpDate = document.getElementById("reportDate");
+    const popUpRef = document.getElementById("referencedReport");
+    const popUpSource = document.getElementById("reportSource");
+    const popUpIndividuals = document.getElementById("reportIndividuals");
+    const popUpPlaces = document.getElementById("reportPlaces");
+    const popUpDates = document.getElementById("reportDates");
+    const popUpOrgs = document.getElementById("reportOrgs");
+    const popUpDescript = document.getElementById("reportDescription");
+
+    popUpTitle.textContent = (`Report ${report.ID}`);
+    popUpDate.textContent = (report.REPORTDATE);
+    if (report.REFERENCEID){
+        popUpRef.textContent = (report.REFERENCEID);
+    }
+    popUpSource.textContent = (report.REPORTSOURCE);
+    popUpIndividuals.textContent = (report.PERSONS);
+    popUpPlaces.textContent = (report.PLACES);
+    popUpDates.textContent = (report.DATES);
+    popUpOrgs.textContent = (report.ORGANIZATIONS);
+    popUpDescript.textContent = (report.REPORTDESCRIPTION);
 }
 
 function getPersonsFromJSON(data) { // Fill the map of suspects from the data
     var personMap = new Map();
 
     data.forEach(report => {
+        fullReports.set(report.ID,report);
         report.PERSONS.forEach(person => {
             if (!personMap.has(person)) {
                 personMap.set(person,1);
@@ -93,26 +138,6 @@ function getPersonsFromJSON(data) { // Fill the map of suspects from the data
     return personMap
 }
 
-// function checkForName(suspect, names){ // Checks for the name in a report
-//     var found = false;
-//     names.forEach( individual => {
-//         if (!found && individual == suspect) {
-//             found = true;
-//         }
-//     });
-//     return found;
-// }
-
-// function getReportsFromName(name, data){ // Gets the list of reports associate with a suspect
-//     let reportList =[];
-//     data.forEach(report => {
-//         if (checkForName(name, report.PERSONS)){
-//             report.push(report.ID);
-//         }
-//     });
-//     return reportList;
-// }
-
 function setSuspectCount(suspectCount){ // Set the displayed counter of suspects
     const suspectCountLabel = document.getElementById("suspectCounter");
 
@@ -126,6 +151,5 @@ async function run(){ // Essentially the main function
     getJSONData(data);
     addButtonListenersToSuspects();
 }
-
 
 run();
